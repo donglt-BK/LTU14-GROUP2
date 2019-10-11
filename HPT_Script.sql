82c4d15ecf9615dac6a398394fd3aa0080c38b32
tmduc0910 <tmduc0910@gmail.com>
2019-10-11 15:49:21 +0700

    Update SQL script

--- HPT_Script.sql
+++ HPT_Script.sql
@@ -2,55 +2,55 @@ CREATE DATABASE olympia;
 USE olympia;
 
 CREATE TABLE User(
-	Id INT PRIMARY KEY,
+	Id INT PRIMARY KEY AUTO_INCREMENT,
     Username VARCHAR(20) UNIQUE NOT NULL,
     Password VARCHAR(20) NOT NULL,
-    Name VARCHAR(50) NOT NULL,
+    Name VARCHAR(20) NOT NULL,
     Gender INT NOT NULL,
-    Balance INT DEFAULT 0
+    Balance INT DEFAULT 50000
 );
 
 CREATE TABLE PlayerData(
 	Id INT NOT NULL,
-    CurrentLevel INT DEFAULT 0,
-    PointTotal INT DEFAULT 0,
+    Current_Level INT DEFAULT 0,
+    Point_Total INT DEFAULT 0,
     FOREIGN KEY (Id) REFERENCES User(Id)
 );
 
 CREATE TABLE Room(
-	Id INT PRIMARY KEY,
-    MaxUsers INT NOT NULL,
-    BetValue INT NOT NULL,
-    MaxQuestions INT NOT NULL
+	Id INT PRIMARY KEY AUTO_INCREMENT,
+    Max_Users INT NOT NULL,
+    Bet_Value INT NOT NULL,
+    Max_Questions INT NOT NULL
 );
 
 CREATE TABLE RoomPlayer(
-	RoomId INT NOT NULL,
-    PlayerId INT NOT NULL,
-    FOREIGN KEY (RoomId) REFERENCES Room(Id),
-    FOREIGN KEY (PlayerId) REFERENCES PlayerData(Id)
+	Room_Id INT NOT NULL,
+    Player_Id INT NOT NULL,
+    FOREIGN KEY (Room_Id) REFERENCES Room(Id),
+    FOREIGN KEY (Player_Id) REFERENCES PlayerData(Id)
 );
 
 CREATE TABLE Topic(
-	Id INT PRIMARY KEY,
-    TopicName VARCHAR(50) UNIQUE NOT NULL,
-    TopicDescription VARCHAR(500) DEFAULT ''
+	Id INT PRIMARY KEY AUTO_INCREMENT,
+    Topic_Name VARCHAR(50) UNIQUE NOT NULL,
+    Topic_Description VARCHAR(500) DEFAULT ''
 );
 
 CREATE TABLE QuestionSet(
-	Id INT PRIMARY KEY,
-    TopicId INT NOT NULL,
+	Id INT PRIMARY KEY AUTO_INCREMENT,
+    Topic_Id INT NOT NULL,
     Difficulty INT NOT NULL,
     Question NVARCHAR(1000) UNIQUE NOT NULL,
-    FOREIGN KEY (TopicId) REFERENCES Topic(Id)
+    FOREIGN KEY (Topic_Id) REFERENCES Topic(Id)
 );
 
 CREATE TABLE Answer(
-	AnswerId INT PRIMARY KEY,
-	QuestionId INT NOT NULL,
+	Answer_Id INT PRIMARY KEY AUTO_INCREMENT,
+	Question_Id INT NOT NULL,
     Answer NVARCHAR(100) NOT NULL,
-    IsCorrect BIT NOT NULL,
-    FOREIGN KEY (QuestionId) REFERENCES QuestionSet(Id)
+    Is_Correct BIT NOT NULL,
+    FOREIGN KEY (Question_Id) REFERENCES QuestionSet(Id)
 );
 
 
