9a4790375a18878cee8f54a9c1e4ca53e13161c7
PurpleLover <sepan223@gmail.com>
2019-11-04 20:06:16 +0700

    Added invitePlayer action

--- src/main/java/com/bk/olympia/UI/screen/HomeScreen.java
+++ src/main/java/com/bk/olympia/UI/screen/HomeScreen.java
@@ -6,11 +6,14 @@ import javax.swing.*;
 import java.awt.*;
 
 import static com.bk.olympia.config.Constant.*;
+import static com.bk.olympia.config.MultiUseFunc.*;
 
 public class HomeScreen extends Screen {
     private static JButton findBtn = new JButton("Find match");
     private static JButton inviteBtn = new JButton("Invite a player");
-    private static JButton logoutBtn = new JButton("Logout");
+    private static JButton logoutBtn = new JButton("Sign out");
+    private static JLabel welcomeLabel = new JLabel("Welcome, fucker!");
+    private static JOptionPane invitePane = new JOptionPane();
 
     public HomeScreen() {
         super(HOME_SCREEN);
@@ -22,6 +25,8 @@ public class HomeScreen extends Screen {
         this.setBounds(0, 0, windowWidth, windowHeight);
         this.setLayout(null);
         this.setBackground(Color.gray);
+
+        welcomeLabel.setBounds(230, 250, 140, 30);
 
         findBtn.setBounds(230, 300, 140, 30);
         findBtn.setEnabled(true);
@@ -35,14 +40,26 @@ public class HomeScreen extends Screen {
         this.add(findBtn);
         this.add(inviteBtn);
         this.add(logoutBtn);
+        this.add(welcomeLabel);
+        this.add(invitePane);
 
         logoutBtn.addActionListener(e -> checkLogout());
+        inviteBtn.addActionListener(e -> onInvitePlayer(this));
+    }
+
+    private void onInvitePlayer(HomeScreen that) {
+        String playerId = invitePane.showInputDialog(that, "Enter player Id: ", "Invite Your Friend", JOptionPane.OK_CANCEL_OPTION);
+        if (!isNullOrEmpty(playerId)) {
+            invitePlayer(playerId);
+        }
+    }
+    private void invitePlayer(String playerId) {
+        System.out.println(playerId);
     }
 
     private void checkLogout() {
         logout();
     }
-
     private void logout() {
         try{
             ui.showScreen(LOGIN_SCREEN);
