7c0072a0fd06e028870a904cc18d5f26ab9e47b0
PurpleLover <sepan223@gmail.com>
2019-11-26 13:50:37 +0700

    init javafx

--- src/main/java/com/bk/olympia/OlympiaClient.java
+++ src/main/java/com/bk/olympia/OlympiaClient.java
@@ -1,30 +1,34 @@
 package com.bk.olympia;
 
-import com.bk.olympia.UI.JFrameUI;
-import com.bk.olympia.UI.ScreenService;
-import com.bk.olympia.exception.ScreenNotFoundException;
-import com.bk.olympia.message.ErrorMessage;
-import com.bk.olympia.request.socket.ResponseHandler;
-import com.bk.olympia.request.socket.SocketService;
-import com.bk.olympia.type.ErrorType;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaders;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.lang.reflect.Type;
+public class OlympiaClient extends Application {
 
-import static com.bk.olympia.config.Constant.*;
+//	public static void main(String[] args) {
+//		JFrameUI ui = JFrameUI.getInstance().config(APP_NAME, WINDOW_WIDTH, WINDOW_HEIGHT);
+//		ui.addScreen(ScreenService.getScreens());
+//
+//		try {
+//			ui.showScreen(LOGIN_SCREEN);
+//		} catch (ScreenNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
 
-public class OlympiaClient {
-
-	public static void main(String[] args) {
-		JFrameUI ui = JFrameUI.getInstance().config(APP_NAME, WINDOW_WIDTH, WINDOW_HEIGHT);
-		ui.addScreen(ScreenService.getScreens());
-
-		try {
-			ui.showScreen(LOGIN_SCREEN);
-		} catch (ScreenNotFoundException e) {
-			e.printStackTrace();
-		}
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("UIFx/sample.fxml"));
+		primaryStage.setTitle("Hello World");
+		primaryStage.setScene(new Scene(root, 300, 275));
+		primaryStage.show();
 	}
 
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
---
+++ src/main/java/com/bk/olympia/UIFx/Controller.java
@@ -0,0 +1,9 @@
+package com.bk.olympia.UIFx;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void pressBtn(ActionEvent event){
+        System.out.print("Hello World");
+    }
+}
