9d253b44c669819590e332eee0df79fbefb97d78
PurpleLover <sepan223@gmail.com>
2019-12-04 17:10:09 +0700

    Added readyBtn logic

--- src/main/java/com/bk/olympia/UIFx/LobbyController.java
+++ src/main/java/com/bk/olympia/UIFx/LobbyController.java
@@ -1,11 +1,22 @@
 package com.bk.olympia.UIFx;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class LobbyController extends ScreenService {
     public Label your_ready;
+    public Button readyBtn;
 
-    public void onPressReady(){
-        your_ready.setVisible(true);
-}
+    public void onPressReady(ActionEvent event) {
+        Boolean ready = your_ready.isVisible();
+        if (!ready) {
+            your_ready.setVisible(true);
+            readyBtn.setText("Not Ready?");
+        } else {
+            your_ready.setVisible(false);
+            readyBtn.setText("Ready!");
+        }
+        //TODO: add send ready func
+    }
 }
