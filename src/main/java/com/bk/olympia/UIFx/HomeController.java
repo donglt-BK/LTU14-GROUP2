3ace266509c9edd41e352d8ea1bd4fbccfd4e65f
donglt <ltdong21197@gmail.com>
2019-12-04 16:17:36 +0700

    update UX

--- src/main/java/com/bk/olympia/UIFx/HomeController.java
+++ src/main/java/com/bk/olympia/UIFx/HomeController.java
@@ -8,29 +8,36 @@ import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
 
 import javafx.event.ActionEvent;
+import javafx.scene.text.Text;
+
 import java.util.Optional;
 
 import static com.bk.olympia.config.Constant.LOGIN_SCREEN;
 
-public class HomeController extends ScreenService{
+public class HomeController extends ScreenService {
     @FXML
     TextField playerId;
 
-    public void findPlayer(){
+    @FXML
+    Text errorMessage;
+
+    public void findPlayer() {
         System.out.println("Finding another player...");
         Platform.runLater(()->{
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("Find random player");
             alert.setTitle("Finding another player, please wait...");
-                                
+
         });
     }
-    public void invitePlayer(){
+
+    public void invitePlayer() {
         System.out.println("Waiting for response...");
     }
-    public void signOut(ActionEvent event){
-        Platform.runLater(()->{
-            try{
+
+    public void signOut(ActionEvent event) {
+        Platform.runLater(() -> {
+            try {
                 Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
                 alert.setTitle("Sign Out");
                 alert.setHeaderText("Signing out of the game");
@@ -43,13 +50,16 @@ public class HomeController extends ScreenService{
 
                 Optional<ButtonType> option = alert.showAndWait();
 
-                if(option.get() == signOut){
+                if (option.get() == signOut) {
                     changeScreen(event, LOGIN_SCREEN);
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
+    }
+
+    public void hideError() {
+        errorMessage.setText("");
     }
 }
